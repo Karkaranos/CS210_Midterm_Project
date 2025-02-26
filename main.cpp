f97593296d6ff746c1ae586f292b31598bd9e664
Karkaranos <karkaranosyt@gmail.com>
2025-02-25 22:41:09 -0600

    Struct and Classes
    - created struct School to store variables
    - created class SchoolList and started function implementation
    - imported class CSVReader

--- main.cpp
+++ main.cpp
@@ -1,6 +1,62 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
-int main() {
-    cout << "Test" << endl;
+struct School {
+    string name;
+    string address;
+    string city;
+    string state;
+    string country;
+    School* next;
+
+    School(string schoolName, string schoolAddress, string schoolCity, string schoolState, string schoolCountry) {
+        name = schoolName;
+        address = schoolAddress;
+        city = schoolCity;
+        state = schoolState;
+        country = schoolCountry;
+        next = nullptr;
+    }
+};
+
+class SchoolList {
+    School* head;
+public:
+    SchoolList() : head(nullptr) {}
+
+    void insertFirst(School school)
+    {
+        School *temp = head;
+        head = new School(school);
+        head->next = temp;
+    }
+
 }
+
+class CSVReader {
+public:
+    static vector<vector<string>> readCSV(const string& filename) {
+        ifstream file(filename);
+        vector<vector<string>> data;
+        string line, word;
+
+        if (!file.is_open()) {
+            cerr << "Error: Could not open file " << filename << endl;
+            return data;
+        }
+
+        while (getline(file, line)) {
+            stringstream ss(line);
+            vector<string> row;
+            while (getline(ss, word, ',')) {
+                row.push_back(word);
+            }
+            data.push_back(row);
+        }
+        file.close();
+        return data;
+    }
+};
